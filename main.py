8d85329d3b719b8a9eefcf36748cca5b389c67e5
Maxime Bedard <maxime.bedard@shopify.com>
2015-07-06 18:35:57 -0400

    added stuff

---
+++ main.py
@@ -0,0 +1,6 @@
+import evaluator
+import trainer
+
+if __name__ == '__main__':
+  trainer = Trainer('input.csv')
+  trainer.train()
---
+++ trainer.py
@@ -0,0 +1,7 @@
+class Trainer:
+  def __init__(self, input):
+    pass
+
+  def train():
+    print('weuriweiruyweir')
+    pass
