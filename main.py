1672eca12131c06248e5f22e42a050c95c1ba0cc
Maxime Bedard <maxime.bedard@shopify.com>
2015-07-20 19:16:31 -0400

    Attempt of fixing everything

--- main.py
+++ main.py
@@ -19,23 +19,23 @@ def normalizeVectors(vectors, high=1.0, low=0.0):
 
 class Neuron:
   def __init__(self, nbInputs):
-    self.weights     = np.random.rand(nbInputs + 1) * 0.5
+    self.weights     = np.random.uniform(-0.5, 0.5, nbInputs + 1)
     self.lastDelta   = np.zeros(nbInputs + 1)
     self.derivatives = np.zeros(nbInputs + 1)
     self.delta       = 0
 
   def activate(self, vector):
     summ = self.weights[-1] * 1.0
-    for (i, input) in enumerate(vector):
-      summ += self.weights[i] * input
+    for (i, elem) in enumerate(vector):
+      summ += self.weights[i] * elem
 
     self.activation = summ
 
   def transfer(self):
     self.output = transferFunc(self.activation)
 
-  def transferDerivative(self):
-    return transferFunc(self.output, derivative=True)
+  def transferDerivative(self, error):
+    self.delta = error * transferFunc(self.output, derivative=True)
 
 class NeuralNetwork:
   def __init__(self, domain, nbInputs, learningRate=0.3, nbNodes=4, iterations=2000, seed=42, momentum=0.8):
@@ -56,50 +56,50 @@ class NeuralNetwork:
     print("Topology : {} {}".format(self.nbInputs, reduce(lambda m,i: m + "{} ".format(str(len(i))), self.network, "")))
 
   def forwardPropagate(self, vector):
-    input = vector
+    elem = vector
     for (i, layer) in enumerate(self.network):
       if i > 0:
-        input = np.array([self.network[i-1][k].output for k in range(len(self.network[i-1]))])
+        elem = np.array([self.network[i-1][k].output for k in range(len(self.network[i-1]))])
 
       for neuron in layer:
-        neuron.activate(input)
+        neuron.activate(elem)
         neuron.transfer()
 
     return self.network[-1][0].output
 
   def backwardPropagateError(self, expected):
     networkLength = len(self.network)
-    for i in range(networkLength):
-      index = networkLength - 1 - i
+    for n in range(networkLength):
+      index = networkLength - 1 - n
 
       if index == networkLength - 1:
         neuron = self.network[index][0] # only one node in output layer
         error  = (expected - neuron.output)
-        neuron.delta = error * neuron.transferDerivative()
+        neuron.transferDerivative(error)
       else:
         for (j, neuron) in enumerate(self.network[index]):
           summ = 0.0
           for nextNeuron in self.network[index+1]:
-            summ += nextNeuron.weights[j] * nextNeuron.delta
+            summ += (nextNeuron.weights[j] * nextNeuron.delta)
 
-          neuron.delta = summ * neuron.transferDerivative()
+          neuron.transferDerivative(summ)
 
   def calculateErrorDerivativesForWeights(self, vector):
-    input = vector
+    elem = vector
     for (i, layer) in enumerate(self.network):
       if i > 0:
-        input = np.array([self.network[i-1][k].output for k in range(len(self.network[i-1]))])
+        elem = np.array([self.network[i-1][k].output for k in range(len(self.network[i-1]))])
 
       for neuron in layer:
-        for (j, signal) in enumerate(input):
+        for (j, signal) in enumerate(elem):
           neuron.derivatives[j] += neuron.delta * signal
 
-        neuron.derivatives[-1] += neuron.delta
+        neuron.derivatives[-1] += neuron.delta * 1.0
 
   def updateWeights(self):
     for layer in self.network:
       for neuron in layer:
-        for (i, weight) in enumerate(neuron.weights):
+        for i in range(len(neuron.weights)):
           delta = (self.learningRate * neuron.derivatives[i]) + (neuron.lastDelta[i] * self.momentum)
           neuron.weights[i] += delta
           neuron.lastDelta[i] = delta
@@ -116,10 +116,11 @@ class NeuralNetwork:
         if round(output) == int(expected):
           correct += 1
 
-        self.backwardPropagateError(expected) # something fucks up here
-        self.calculateErrorDerivativesForWeights(vector) # or here?
+        self.backwardPropagateError(expected)
+        self.calculateErrorDerivativesForWeights(vector)
 
       self.updateWeights()
+      #print("CORRECT = {0}".format(correct))
       nextEpoch = epoch + 1
       if nextEpoch % 100 == 0:
         print("epoch={}, correct={}/{}".format(nextEpoch, correct, 100*len(self.domain)))
