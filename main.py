ab3735163719a462becd961c8f5b0fe458f2904f
Maxime Bedard <maxime.bedard@shopify.com>
2015-07-25 15:11:28 -0400

    Major refactor :tm:

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import csv, math, itertools
 import numpy as np
 from sortedcontainers import SortedListWithKey
 from math import sqrt
-import pdb, sys
+import ipdb, sys
 
-def euclideanDistance(v1, v2):
-  return math.sqrt(sum([pow(v1[i] - v2[i], 2) for i in range(len(v1))]))
+def euclideanDistance(p, q):
+  return sum([pow(q[i] - p[i], 2) for i, value in enumerate(p)])
 
 def majority(klasses):
   return max(set(klasses), key=klasses.count)
+
+def ponderate(klasses):
+  a = sum(map(lambda x: x[1].label/ x[0], klasses))
+  b = sum(map(lambda x: 1 / x[0], klasses))
+  return int(a/b)
 
 def normalize(vectors, high=1.0, low=0.0, ignore=-1):
   mins = np.min(vectors, axis=0)
@@ -20,20 +25,24 @@ def normalize(vectors, high=1.0, low=0.0, ignore=-1):
 
 def loadMatrix(file):
   matrix = np.genfromtxt(file, names=True, delimiter=";")
-  return matrix.view(np.float64).reshape(vectors.shape + (-1,))
+  return matrix.view(np.float64).reshape(matrix.shape + (-1,))
 
 class Knn(object):
-  def __init__(self, training, testing, distanceFunc = euclideanDistance):
-    self.training     = training
-    self.testing      = testing
-    self.distanceFunc = distanceFunc
+  def __init__(self, training, testing,
+               distanceFunc = euclideanDistance,
+               approximateFunc = majority):
+
+    self.training        = training
+    self.testing         = testing
+    self.distanceFunc    = distanceFunc
+    self.approximateFunc = approximateFunc
 
     self.preprocess()
 
   def preprocess(self):
     trainingMat = loadMatrix(self.training)
     testingMat  = loadMatrix(self.testing)
-    allMat      = trainingMat + testingMat
+    allMat      = np.concatenate((trainingMat, testingMat))
 
     normalizedMat = normalize(allMat)
     end           = len(trainingMat)
@@ -42,7 +51,7 @@ class Knn(object):
     self.examples     = normalizedMat[end:]
     self.nbAttributes = len(self.facts[0])
 
-  def nearestsClasses(self, fact, examples):
+  def nearestsClasses(self, k, fact, examples):
     nearests = SortedListWithKey(key = lambda val: val[0])
 
     for example in examples:
@@ -56,19 +65,24 @@ class Knn(object):
 
     return nearests[-k:]
 
-  def applyFeatureExtraction(self):
-    pass
-
-  def accuracy(self, facts, examples):
+  def accuracy(self, k, facts, examples):
+    correct = 0
     for fact in facts:
-      nn = self.nearestsClasses(fact, examples)
-      
+      expected = fact[-1]
+      nn      = self.nearestsClasses(k, fact, examples)
+      classes = list(map(lambda x: x[1], nn))
+      output  = self.approximateFunc(classes)
 
-    pass
+      ipdb.set_trace()
+      if output == expected:
+        correct += 1
+
+    return correct / len(facts)
 
   def run(self, k):
-
+    result = self.accuracy(k, self.facts, self.examples)
+    print(result)
 
 if __name__ == '__main__':
-  trainer = Trainer('dataset.csv', 'sample.csv')
-  print(trainer.train())
+  knn = Knn('dataset.csv', 'sample.csv')
+  knn.run(5)
