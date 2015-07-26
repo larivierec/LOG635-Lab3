a6bf8e20ddbfb22d1a9c30fdc96d3b65ae3c0183
Maxime Bedard <maxime.bedard@shopify.com>
2015-07-26 17:28:45 -0400

    final tweaks since i'm annoyed by this

--- main.py
+++ main.py
@@ -30,24 +30,24 @@ def loadMatrix(file):
   return matrix.view(np.float64).reshape(matrix.shape + (-1,))
 
 class Knn(object):
-  def __init__(self, training, testing,
+  def __init__(self, facts, examples,
                distanceFunc = euclideanDistance,
                approximateFunc = majority):
 
-    self.training        = training
-    self.testing         = testing
+    self.facts           = facts
+    self.examples        = examples
     self.distanceFunc    = distanceFunc
     self.approximateFunc = approximateFunc
 
     self.preprocess()
 
   def preprocess(self):
-    trainingMat = loadMatrix(self.training)
-    testingMat  = loadMatrix(self.testing)
-    allMat      = np.concatenate((trainingMat, testingMat))
+    factsMat    = loadMatrix(self.facts)
+    examplesMat = loadMatrix(self.examples)
+    allMat      = np.concatenate((factsMat, examplesMat))
 
     normalizedMat = normalize(allMat)
-    end           = len(trainingMat)
+    end           = len(factsMat)
 
     self.facts        = normalizedMat[0:end]
     self.examples     = normalizedMat[end:]
@@ -62,54 +62,66 @@ class Knn(object):
       exampleV     = example[0:-1]
       exampleClass = example[-1]
 
+      if fact is example:
+        continue
+
       distance = self.distanceFunc(exampleV, factV)
       nearests.add((distance, factClass, factV))
 
     return nearests[:k]
 
-  def accuracy(self, k, facts, examples):
+  def accuracy(self, k, facts, examples, printApproximation=False):
     correct = 0
     for example in examples:
       expected = example[-1]
       nn       = self.nearestsNeighbours(k, facts, example)
       output   = self.approximateFunc(nn)
 
-      # classes = list(map(lambda x: x[1], nn))
-      # print("{} => {} == {}".format(classes, output, expected))
+      if printApproximation:
+        print("{} => {}".format(example, output))
 
-      if output == expected:
+      if expected != -1 and output == expected:
         correct += 1
 
     return correct / len(examples)
 
   def backwardElimination(self, k):
     accuracy = 0
-    ignored = []
+    unrelevant = []
+    print("Applying backward elimination to find unrelevant attributes")
     for i in range(self.nbAttributes - 1):
-      ignored.append(i)
+      print("{}% completed".format(int(((i / (self.nbAttributes - 1)) * 100))))
+      unrelevant.append(i)
 
-      facts    = np.delete(self.facts, ignored, 1)
-      examples = np.delete(self.examples, ignored, 1)
-
-      output = self.accuracy(k, facts, examples)
-      print("ignoring {}, accuracy {} for {}".format(i, output, ignored))
+      facts = np.delete(self.facts, unrelevant, 1)
+      output = self.accuracy(k, facts, facts)
 
       if output > accuracy:
         accuracy = output
       else:
-        ignored.remove(i)
+        unrelevant.remove(i)
 
-    return ignored
+    print("Attributes at index {} seems unrelevant".format(unrelevant))
+    return unrelevant
 
   def run(self, k):
-    ignored = self.backwardElimination(k)
-    result  = self.accuracy(k,
-      np.delete(self.facts, ignored, 1),
-      np.delete(self.examples, ignored, 1))
+    print("Using kNN with k={}".format(k))
 
-    return (k, result)
+    # Find the attributes that are not all that useful
+    #unrelevant = self.backwardElimination(k)
+    unrelevant = [0, 5, 6]
+
+    # Remove the useless attributes
+    examples = np.delete(self.examples, unrelevant, 1)
+    facts    = np.delete(self.facts, unrelevant, 1)
+
+    # We print the approximation only
+    accuracy = self.accuracy(k, facts, examples, printApproximation=True)
+
+    # If there is an actual quality to be evaluated, an accuracy will be > 0
+    if accuracy > 0:
+      print("Accuracy = {}".format(accuracy))
 
 if __name__ == '__main__':
-  p = Pool(4)
   knn = Knn('dataset.csv', 'sample.csv')
-  print(p.map(knn.run, range(2,30)))
+  knn.run(5)
